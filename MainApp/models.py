ea0633ba4d4856d7a5d4189c8c1d15b814a28dd8
GitDgon <dgon_mail@mail.ru>
2022-12-03 11:36:48 +0300

    Две базы

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 
-class Snippet(models.Model):
+class Svs_k(models.Model):
+    name = models.CharField(max_length=100)
+    lang = models.CharField(max_length=30)
+    code = models.TextField(max_length=5000)
+    creation_date = models.DateTimeField(auto_now=True)
+
+class Svs_z(models.Model):
     name = models.CharField(max_length=100)
     lang = models.CharField(max_length=30)
     code = models.TextField(max_length=5000)
